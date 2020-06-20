b1b0103e58e1e9c9214db1ff5c46ffc83bc40b22
Luo27565 <luo27565@gmail.com>
2020-06-20 17:04:50 +0800

    blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	base: 'blog',
+    title: 'Little铵',
+    description: '迷途漫漫，终有一归。',
+    themeConfig: {
+  		nav: [{text: "Home", link: "/"},
+      		  {text: "Life", link: "/life/"},
+      		  {text: "Happy", link: "/happy/"},
+      		  {text: "Fun", link: "/fun/"},
+      		  {text: "Love", link: "/love/"},
+      		  {text: "Study", link: "/study/"}],
+      	sidebar: 'auto',
+	},
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+---
+home: true
+Image: /home.png
+actionText: Get Started →
+actionLink: /node/
+
+footer: MIT Licensed | Copyright © 2020-present Luo
+---
